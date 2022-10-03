551425afc228c5d5f740709e40b92fd0e58816d2
DESKTOP-RE8LH2A\Shprgd <isephar@gmail.com>
2022-10-03 09:12:09 +0300

    fix cors

--- server/controller/main.controller.js
+++ server/controller/main.controller.js
@@ -1,7 +1,18 @@
-const db = require("../db");
+const db = require('../db');
 
 class MainController {
   async getTable(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    ); // If needed
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With,content-type'
+    ); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
     const table = await db.query(`SELECT * FROM public.items `);
     res.json(table.rows);
   }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const PORT = process.env.PORT || 8080;
-const mainRouter = require("./routes/main.routes");
-const cors = require("cors");
+const mainRouter = require('./routes/main.routes');
+const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
-app.use("/api", mainRouter);
+app.use(cors({ origin: '*' }));
+app.use('/api', mainRouter);
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
